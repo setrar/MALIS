a027f823d96637b256304140e4aa142371c78d41
setrar <setrar@valiha.com>
2023-11-09 18:08:21 +0100

    .

---
+++ projects/proj1/REPORT.md
@@ -0,0 +1,21 @@
+# REPORT
+
+## Part I – Implementing a kNN from scratch:
+
+- [ ] Task 1.
+
+By updating the `knn.py` source file representing the kNN Classifier, I was able to add/fill the nescessary methods that allows the `experiment` notebook to run.
+
+The `knn.py` source code mimics the `sklearn.neighbors.KNeighborsClassifier` from the Python package `scikit-learn `. 
+
+The distance metrics used are `Euclidian distance` and `Minkowski distance` using the same function but changing the parameters 
+
+```math
+D(X,Y) = ( \sum_{i=1}^n | x_i - y_i |^p )^{\dfrac{1}{p}}
+```
+
+- [ ] Task 2.
+
+Studying the `curse of dimensionality` issue, I figured there was no need to exceed the 300 neighbor count so I tested through the notebook by ,first, adding manual code and then looping through a function.
+
+The loop returned with an accuracy score of 80.54% at 246 neighbor point. 
--- projects/proj1/knn.py
+++ projects/proj1/knn.py
@@ -1,118 +1,41 @@
-from scipy.spatial import distance_matrix
-from scipy.spatial import minkowski_distance as m_d
-from collections import Counter
 import numpy as np
-from math import sqrt
+from collections import Counter
+from scipy.spatial import minkowski_distance as m_d
 
-"""
-# https://machinelearningmastery.com/distance-measures-for-machine-learning/ 
-# calculate minkowski distance
-"""
-#def minkowski_distance(a, b, p):
-#return sum(abs(e1-e2)**p for e1, e2 in zip(a,b))**(1/p)
-
-def minkowski_distance(a, b, p):
-    return sum(distance(e1, e2, p) for e1, e2 in zip(a,b))
-
-def distance(e1, e2, p):
-    distance = (np.sum(abs(e1-e2)**p))**(1/p)
+def minkowski_distance(x1, x2, p=2):
+    distance = (np.sum(abs(x1 - x2)**p))**(1/p)
     return distance
 
-
 class KNN:
-    '''
-    k nearest neighboors algorithm class
-    __init__() initialize the model
-    train() trains the model
-    predict() predict the class for a new point
-    '''
-
-    def __init__(self, k):
-        '''
-        INPUT :
-        - k : is a natural number bigger than 0 
-        '''
-
-        if k <= 0:
-            raise Exception("Sorry, no numbers below or equal to zero. Start again!")
-            
-        # empty initialization of X and y
-        self.X = []
-        self.y = []
-        # k is the parameter of the algorithm representing the number of neighborhoods
+    def __init__(self, k=3):
         self.k = k
-        
-    def train(self,X,y):
-        '''
-        INPUT :
-        - X : is a 2D NxD numpy array containing the coordinates of points
-        - y : is a 1D Nx1 numpy array containing the labels for the corrisponding row of X
-        '''        
+
+    def fit(self, X, y):
         self.X_train = X
         self.y_train = y
 
-    def predict(self,X_new,p):
-        '''
-        INPUT :
-        - X_new : is a MxD numpy array containing the coordinates of new points whose label has to be predicted
-        A
-        OUTPUT :
-        - y_hat : is a Mx1 numpy array containing the predicted labels for the X_new points
-        ''' 
-        predictions = [self._predict(x, p) for x in X_new]
+    def predict(self, X):
+        predictions = [self._predict(x,p=2) for x in X]
+        return predictions
+
+    def minkowski_prediction(self, X, p):
+        predictions = [self._predict(x, p) for x in X]
         return predictions
 
     def _predict(self, x, p):
         # compute the distance
-        distances = self.minkowski_dist(x, p)
-
+        distances = [minkowski_distance(x, x_train, p) for x_train in self.X_train]
+    
         # get the closest k
         k_indices = np.argsort(distances)[:self.k]
         k_nearest_labels = [self.y_train[i] for i in k_indices]
 
         # majority vote
-        y_hat = Counter(k_nearest_labels).most_common()
-        return y_hat[0][0]
-    
-    def minkowski_dist(self,X_new,p):
-        '''
-        INPUT : 
-        - X_new : is a MxD numpy array containing the coordinates of points for which the distance to the training set X will be estimated
-        - p : parameter of the Minkowski distance
-        
-        OUTPUT :
-        - dst : is an MxN numpy array containing the distance of each point in X_new to X
-        '''
-        dst = minkowski_distance( X_new, self.X_train, p)
-        
-        return dst
+        most_common = Counter(k_nearest_labels).most_common()
+        return most_common[0][0]
 
-"""
-Testing: 
-Compare minkowski_distance with scipy.spatial
-"""
-def main():
-    # define data
-    row1 = [10, 20, 15, 10, 5]
-    row2 = [12, 24, 18, 8, 7]
-    # calculate distance (p=1)
-    print('manhattan')
-    dist = minkowski_distance(row1, row2, 1); print(dist)
-    dist = m_d(row1, row2, 1); print(dist)
-    # calculate distance (p=2)
-    print('euclidian')
-    dist = minkowski_distance(row1, row2, 2); print(dist)
-    dist = m_d(row1, row2, 2); print(dist)
-    print('Object')
-    k = KNN(1)
-    k.train(row1,row2)
-    dist = k.minkowski_dist(row2,1)
-
-    print('Individual')
-    # euclidian
-    print(distance(12, 10, 2))
-    # manhattan
-    print(distance(13, 10, 1))
-
-if __name__ == "__main__":
-    main()
+    # from sklearn.metrics import accuracy_score
+    # accuracy_score(y_test, y_pred)
+    def accuracy_score(self, y_test, y_pred):
+        acc = np.sum(y_pred == y_test) / len(y_test)
+        return acc
