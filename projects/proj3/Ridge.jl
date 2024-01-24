9878d935a9a95fec2f480f37662d4f51acf61e66
setrar <setrar@valiha.com>
2024-01-24 23:50:06 +0100

    .

--- projects/proj3/Ridge.jl
+++ projects/proj3/Ridge.jl
@@ -1,28 +1,30 @@
 ##------------------------------------------------------------------------------
 """
-    StandardiseYX(Y0,X0)
+    Standardise(y₀,X₀)
 
-Demean and make std=1 for `Y` and `X` (vector or matrices)
+Demean and make std=1 for `y` and `X` (vector or matrices)
 
 """
-function StandardiseYX(Y0,X0)
-    Y = (Y0 .- mean(Y0,dims=1))./std(Y0,dims=1)
-    X = (X0 .- mean(X0,dims=1))./std(X0,dims=1)
-    return Y,X
+function Standardise(y₀,X₀)
+    y = (y₀ .- mean(y₀,dims=1))./std(y₀,dims=1)
+    X = (X₀ .- mean(X₀,dims=1))./std(X₀,dims=1)
+    return y,X
 end
 ##------------------------------------------------------------------------------
 
 
 ##------------------------------------------------------------------------------
 """
-    RidgeRegression(Y,X,λ,β₀=0)
+    RidgeRegression(y,X,λ,β₀=0)
 
 Calculate ridge regression estimate with target vector `β₀`.
 """
-function RidgeRegression(Y,X,λ,β₀=0)
+function RidgeRegression(y,X,λ,β₀=0)
     (T,K) = (size(X,1),size(X,2))
     isa(β₀,Number) && (β₀=fill(β₀,K))
-    b = (X'X/T+λ*I)\(X'Y/T+λ*β₀)      #same as inv(X'X/T+λ*I)*(X'Y/T+λ*β₀)
+    Cₓₓ = (X)ᵀ*X 
+    Cₓᵧ = (X)ᵀ*y
+    b = (Cₓₓ/T+λ*I)⁻¹*(Cₓᵧ/T+λ*β₀) #same as inv(X'X/T+λ*I)*(X'Y/T+λ*β₀) or (Cₓₓ/T+λ*I)\(Cₓᵧ/T+λ*β₀)
     return b
 end
 ##------------------------------------------------------------------------------
